280cb1ad8dbd5d798ef9453865a757d2f1ce3206
Mehmet Emin <cobanmehmet3525@gmail.comm>
2024-02-22 10:41:16 +0100

    plots added

--- plot.py
+++ plot.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.metrics import mean_absolute_error
 
 data = pd.read_csv("data/properties.csv")
 n_data = data["price"]
@@ -18,3 +19,17 @@ plt.title("Actual vs. Predicted Prices")
 plt.xlabel("Actual Price")
 plt.ylabel("Predicted Price")
 plt.show()
+
+
+data2 = pd.read_csv("data/mae.csv")
+n_data2 = data2["actual_values"]
+n_data3 = data2["predicted_values"]
+n_data4 = data2["difference"]
+sns.scatterplot(x=n_data2, y=n_data3, alpha=0.5)
+plt.show()
+
+sns.pointplot(x=n_data4, y=len(n_data3), alpha=0.5)
+plt.show()
+
+
+
